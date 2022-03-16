4d27d08c43e50ea5f5c6bb985c89afe3797d3a41
Tatsuya Sato <satoryu.1981@gmail.com>
2022-03-16 17:30:34 +0900

    Allows users to add new task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import TodoInputForm from "./TodoInputForm";
 import TodoList from "./TodoList";
+import React from 'react'
 
-function App(props) {
-  const todos = props.todos;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className="App">
-      <TodoInputForm />
-      <TodoList todos={todos} />
-    </div>
-  );
+    this.state = {
+      todos: this.props.todos
+    }
+
+    this.addNewTask = this.addNewTask.bind(this)
+  }
+
+  addNewTask(task) {
+    const todos = this.state.todos
+    todos.push({ task, completed: false })
+
+    this.setState({
+      todos: todos
+    })
+  }
+
+  render() {
+    const todos = this.props.todos;
+
+    return (
+      <div className="App">
+        <TodoInputForm onSubmit={this.addNewTask} />
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TodoInputForm.js
+++ src/TodoInputForm.js
@@ -1,8 +1,34 @@
-export default function TodoInputForm(props) {
-  return (
-    <form>
-      <input name="task" />
-      <input type="submit" value="Create" />
-    </form>
-  );
+import React from 'react'
+
+export default class TodoInputForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      task: ''
+    }
+
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(event) {
+    event.preventDefault();
+    this.setState({ task: event.target.value })
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.task)
+    this.setState({task: ''})
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input type="text" value={this.state.task} onChange={this.onChange} />
+        <input type="submit" value="Add" />
+      </form>
+    );
+  }
 }
